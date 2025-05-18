5ba9394e07124d2330e665e5ccb2c92aaf94f868
AaryanBasnet <basnetaryan1011@gmail.com>
2025-05-18 21:36:56 +0545

    routes added

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:venure/view/splash_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {'/': (context) => SplashScreen()},
+      debugShowCheckedModeBanner: false,
+    );
+    
   }
 }
