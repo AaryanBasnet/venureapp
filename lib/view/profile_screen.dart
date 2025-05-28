eb68b1c2832db4ce4d890ccd048885777f09c758
AaryanBasnet <basnetaryan1011@gmail.com>
2025-05-29 01:03:51 +0545

    Minor profile screen Ui designed

--- lib/view/profile_screen.dart
+++ lib/view/profile_screen.dart
@@ -5,8 +5,42 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Profile page"),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Text(
+              "Profile",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              SizedBox(width: 30),
+              CircleAvatar(),
+              SizedBox(width: 20),
+
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [Text("Aaryan"), Text("Aaryan@gmail.com")],
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Card(
+            child: Column(
+              children: [
+                Text("Edit profile Information"),
+                SizedBox(height: 20),
+                Text("Edit profile Information"),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
