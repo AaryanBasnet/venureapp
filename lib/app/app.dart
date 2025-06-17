da2499f05eec961a35d438a388473e20dc819e7e
AaryanBasnet <basnetaryan1011@gmail.com>
2025-06-17 22:10:35 +0545

    Refactor: move theme_data.dart to app directory and update import paths

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:venure/theme/theme_data.dart';
+import 'package:venure/app/theme/theme_data.dart';
 import 'package:venure/view/dashboard_screen.dart';
 // import 'package:venure/view/home_screen.dart';
 import 'package:venure/view/login_screen.dart';
