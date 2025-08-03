239b0630c6038ce6eb5da7548c663276ff67c69d
AaryanBasnet <basnetaryan1011@gmail.com>
2025-08-03 18:54:33 +0545

    Update search state

--- lib/features/home/presentation/view_model/search_state.dart
+++ lib/features/home/presentation/view_model/search_state.dart
@@ -1,6 +1,11 @@
+import 'package:equatable/equatable.dart';
 import 'package:venure/features/home/domain/entity/venue_entity.dart';
 
-abstract class SearchState {}
+ class SearchState extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+ }
 
 class SearchInitial extends SearchState {}
 
@@ -11,9 +16,13 @@ class SearchLoaded extends SearchState {
   final List<String> favoriteVenueIds;
 
   SearchLoaded(this.venues, {this.favoriteVenueIds = const []});
+  @override
+  List<Object?> get props => [venues, favoriteVenueIds];
 }
 
 class SearchError extends SearchState {
   final String message;
   SearchError(this.message);
+  @override
+  List<Object?> get props => [message];
 }
