1bc8ec0a978b40a2f5105a8edfdab43702f83dbc
AaryanBasnet <basnetaryan1011@gmail.com>
2025-08-03 18:53:56 +0545

    Update register view model

--- lib/features/auth/presentation/view_model/register_view_model/register_view_model.dart
+++ lib/features/auth/presentation/view_model/register_view_model/register_view_model.dart
@@ -1,9 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:venure/core/snackbar/my_snackbar.dart';
 import 'package:venure/features/auth/domain/use_case/user_register_usecase.dart';
-import 'package:venure/features/auth/presentation/view/login_view.dart';
-import 'package:venure/features/auth/presentation/view/login_wrapper.dart';
 import 'package:venure/features/auth/presentation/view_model/register_view_model/register_event.dart';
 import 'package:venure/features/auth/presentation/view_model/register_view_model/register_state.dart';
 
@@ -16,17 +12,7 @@ class RegisterViewModel extends Bloc<RegisterEvent, RegisterState> {
     on<RegisterUserEvent>(_onRegisterUser);
   }
 
-  // void _onNavigateToLoginView(
-  //   NavigateToLoginView event,
-  //   Emitter<RegisterState> emit,
-  // ) {
-  //   if (event.context.mounted) {
-  //     Navigator.push(
-  //       event.context,
-  //       MaterialPageRoute(builder: (_) => LoginWrapper()),
-  //     );
-  //   }
-  // }
+  
 
   Future<void> _onRegisterUser(
     RegisterUserEvent event,
