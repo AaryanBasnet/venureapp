98574af9383ee41d301fd31e8643ad574e6ca845
AaryanBasnet <basnetaryan1011@gmail.com>
2025-08-03 18:54:38 +0545

    Update notification entity

--- lib/features/notification/domain/entity/notification_entity.dart
+++ lib/features/notification/domain/entity/notification_entity.dart
@@ -1,15 +1,27 @@
-class NotificationEntity {
+import 'package:equatable/equatable.dart';
+
+class NotificationEntity extends Equatable {
   final String id;
   final String type;
   final String message;
   final bool isRead;
   final DateTime createdAt;
 
-  NotificationEntity({
+  const NotificationEntity({
     required this.id,
     required this.type,
     required this.message,
     required this.isRead,
     required this.createdAt,
   });
+  
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+    id,
+    type,
+    message,
+    isRead,
+    createdAt,
+  ];
 }
