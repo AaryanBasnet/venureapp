e005dbcc80c1feb3ab1c78f3f81c6680e979c1c0
AaryanBasnet <basnetaryan1011@gmail.com>
2025-08-03 18:54:45 +0545

    Update my bookings screen navigation

--- lib/features/profile/presentation/view/my_bookings_view/my_bookings_screen.dart
+++ lib/features/profile/presentation/view/my_bookings_view/my_bookings_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:venure/app/service_locator/service_locator.dart';
 import 'package:venure/features/booking/domain/use_case/cancel_booking_usecase.dart';
-import 'package:venure/features/home/domain/repository/venue_repository.dart';
 import 'package:venure/features/profile/presentation/view/my_bookings_view/my_bookings_view.dart';
 import 'package:venure/features/profile/presentation/view_model/my_bookings_view_model/my_booking_view_model.dart';
 import 'package:venure/features/profile/presentation/view_model/my_bookings_view_model/my_bookings_event.dart';
